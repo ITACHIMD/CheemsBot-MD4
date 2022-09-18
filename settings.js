35246befa7a3b98b6fbeb7d730a4fe36c4e11d10
ITACHIMD <113094012+ITACHIMD@users.noreply.github.com>
2022-09-18 17:29:27 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -54,32 +54,32 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.vcardowner = ['916909137213'] //ur owner number
-global.ownername = "🦄Dream Guy Xeon" //ur owner name
-global.ytname = "YT: Xeon" //ur yt chanel name
-global.socialm = "GitHub: DGXeon" //ur github or insta name
-global.location = "India, Mizoram, Aizawl" //ur location
+global.vcardowner = ['917736908570'] //ur owner number
+global.ownername = "ī.am      💓҉️      𝙸𝚃𝙰𝙲𝙷𝙸" //ur owner name
+global.ytname = "YT: ITACHIMD" //ur yt chanel name
+global.socialm = "GitHub: _SOUL.__.KID_" //ur github or insta name
+global.location = "India, kerala, palakkad" //ur location
 
 //bot bomdy 
-global.owner = ['916909137213']
-global.ownertag = '916909137213' //ur tag number
-global.botname = 'Cheems Bot MD V4' //ur bot name
-global.linkz = "https://youtu.be/WiIqCdiDjFo" //your theme url which will be displayed on whatsapp
-global.websitex = "https://youtu.be/WiIqCdiDjFo" //ur website to be displayed
+global.owner = ['917736908570']
+global.ownertag = '917736908570' //ur tag number
+global.botname = 'ī.am      💓҉️      𝙸𝚃𝙰𝙲𝙷𝙸' //ur bot name
+global.linkz = "https://youtube.com/channel/UC2DxiDEyDA5JHnucPhe4R3Q" //your theme url which will be displayed on whatsapp
+global.websitex = "https://youtube.com/channel/UC2DxiDEyDA5JHnucPhe4R3Q" //ur website to be displayed
 global.botscript = 'https://github.com/DGXeon/CheemsBot-MD4' //script link
 global.reactmoji = "😘" //ur menu react emoji
 global.themeemoji = "🦄" //ur theme emoji
-global.packname = "Cheems Bot Stickers\n\n\n\n\n\n\n\n\n\n\n🦄🦄🦄 " //ur sticker watermark packname
-global.author = "🦄Dream Guy Xeon" //ur sticker watermark author
+global.packname = "ī.am      💓҉️      𝙸𝚃𝙰𝙲𝙷𝙸" //ur sticker watermark packname
+global.author = "ī.am      💓҉️      𝙸𝚃𝙰𝙲𝙷𝙸" //ur sticker watermark author
 
 //Bot theme media
-global.thum = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
-global.log0 = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur logo pic
-global.err4r = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur error pic
-global.thumb = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
+global.thum = https://bbts1.azureedge.net/images/p/full/2022/06/7de035b8-45a7-4a3c-88ee-98b6ec8487fe.jpg //ur thumb pic
+global.log0 = https://bbts1.azureedge.net/images/p/full/2022/06/7de035b8-45a7-4a3c-88ee-98b6ec8487fe.jpg //ur logo pic
+global.err4r = https://bbts1.azureedge.net/images/p/full/2022/06/7de035b8-45a7-4a3c-88ee-98b6ec8487fe.jpg //ur error pic
+global.thumb = https://bbts1.azureedge.net/images/p/full/2022/06/7de035b8-45a7-4a3c-88ee-98b6ec8487fe.jpg //ur thumb pic
 
 //damtabase
-global.premium = ['916909137213'] //ur premium numbers
+global.premium = ['917736908570'] //ur premium numbers
 global.ntilinkytvid = []
 global.ntilinkytch = []
 global.ntilinkig = []
